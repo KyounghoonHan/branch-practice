97f0c46d05fd5c65a093cf3203ec716f5d95323e
{KyounghoonHan} <{khhan7813@gmail.com}>
2022-05-06 15:14:47 +0900

    feat: Test conflict

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-for i in range(1, 21):
-    if i % 3 == 0:
+for i in range(1, 25):
+    if i % 5 == 0:
+        print('dream')
+    elif i % 3 == 0:
         print('year')
     else:
         print(i)
